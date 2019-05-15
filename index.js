ba985338e01a88f5257eccca5cb67b4325604c08
DiamondYuan <admin@diamondyuan.com>
2019-05-15 18:56:07 +0800

    ci: init now.json

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-var nodemailer = require("nodemailer");
-var transporter = nodemailer.createTransport({
-  host: "smtpdm.aliyun.com",
-  port: 80,
-  secureConnection: false,
-  auth: {
-    user: "", // user name
-    pass: "" // password
-  }
-});
-
-var mailOptions = {
-  from: "",
-  to: "",
-  subject: "Hello",
-  text: "Hello world",
-  attachments: [
-    {
-      filename: "text0.txt",
-      content: "hello world!"
-    }
-  ]
+module.exports = (req, res) => {
+  res.end(`Hello from Node.js on Now 2.0!`);
 };
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    return console.log(error);
-  }
-  console.log("Message sent: " + info.response);
-});

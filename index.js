4ea6752533d79541e7d698054804c03ffb97f4c3
DiamondYuan <admin@diamondyuan.com>
2019-05-15 19:18:55 +0800

    feat: support send mail

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-module.exports = (req, res) => {
-  res.end(`Hello from Node.js on Now 2.0!`);
+const { json } = require("micro");
+const nodemailer = require("nodemailer");
+
+module.exports = async (req, res) => {
+  const body = await json(req);
+  const {
+    host = "smtpdm.aliyun.com",
+    user,
+    password,
+    from,
+    to,
+    subject,
+    text,
+    attachments
+  } = body;
+
+  const transporter = nodemailer.createTransport({
+    host,
+    port: 80,
+    secureConnection: false,
+    auth: {
+      user,
+      pass: password // password
+    }
+  });
+  const mailOptions = {
+    from,
+    to,
+    subject,
+    text,
+    attachments
+  };
+  await transporter.sendMail(mailOptions);
+  res.end(`success`);
 };
